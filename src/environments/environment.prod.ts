3e1a31da4f3f0d6e82e041cc75a4dbf65d5b9e18
Matthias Mueller <matthiasm@live.de>
2018-04-18 18:38:24 +0200

    Changed redirect uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   adalConfig: {
     tenant: 'novacapta.de',
     clientId: 'f9aa233a-c9fe-493e-b5d9-0ea2cca7b22f',
-    redirectUri: 'http://localhost:4200/',
+    redirectUri: 'https://ngseed-webui.azurewebsites.net/',
     endpoints: {
       'https://ngseed-backend.azurewebsites.net/': '297fc587-359a-4ea5-9f2d-4b36aef9c3a1'
     }
