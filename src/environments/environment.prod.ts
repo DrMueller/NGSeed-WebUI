adcb7cf88da710425567a96046a42db73da25566
Matthias Mueller <matthiasm@live.de>
2018-04-18 17:13:33 +0200

    WIP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
     clientId: 'f9aa233a-c9fe-493e-b5d9-0ea2cca7b22f',
     redirectUri: 'http://localhost:4200/',
     endpoints: {
-      'https://localhost:44392/': '297fc587-359a-4ea5-9f2d-4b36aef9c3a1'
+      'https://ngseed-backend.azurewebsites.net/': '297fc587-359a-4ea5-9f2d-4b36aef9c3a1'
     }
   }
 };
