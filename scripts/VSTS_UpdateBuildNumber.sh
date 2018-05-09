ac6bc90d4ec6206e82bbd43191b6002f1bb8f674
Matthias Mueller <matthiasm@live.de>
2018-05-09 08:06:55 +0200

    WIP

--- scripts/VSTS_UpdateBuildNumber.sh
+++
@@ -1,3 +0,0 @@
-( shopt -s globstar dotglob
-    sed -i -- 's/BUILD_BUILDNUMBER/bar/g' **environment.prod.ts*
-)
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,7 +11,7 @@ export const environment = {
     }
   },
   appInfo: {
-    appVersion: 'BUILD_BUILDNUMBER',
+    appVersion: '__BUILD_BUILDNUMBER__',
     appName: 'NG-Seed'
   }
 };
