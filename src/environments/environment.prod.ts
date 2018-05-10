f02c8afa566a97b6d17191782d0938328bb9bdad
Matthias Mueller <matthiasm@live.de>
2018-05-10 15:07:14 +0200

    WIP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
     clientId: '297fc587-359a-4ea5-9f2d-4b36aef9c3a1',
     redirectUri: 'http://drmueller.westeurope.cloudapp.azure.com:8080/',
     endpoints: {
-      'https://ngseed-backend.azurewebsites.net/': '297fc587-359a-4ea5-9f2d-4b36aef9c3a1'
+      'http://drmueller.westeurope.cloudapp.azure.com:8081/': '297fc587-359a-4ea5-9f2d-4b36aef9c3a1'
     }
   },
   appInfo: {
@@ -15,5 +15,3 @@ export const environment = {
     appName: 'NG-Seed'
   }
 };
-
-
