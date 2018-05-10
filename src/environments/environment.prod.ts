99e5368b0955b2b6b2204e39b1e892b5300123f2
Matthias Mueller <matthiasm@live.de>
2018-05-10 21:22:49 +0200

    WIP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   activateSecurity: true,
   production: true,
-  coreServiceBaseUrl: 'http://drmueller.westeurope.cloudapp.azure.com:8081/api',
+  coreServiceBaseUrl: 'http://drmueller.westeurope.cloudapp.azure.com:80/api',
   adalConfig: {
     tenant: 'novacapta.de',
     clientId: '297fc587-359a-4ea5-9f2d-4b36aef9c3a1',
     redirectUri: 'http://drmueller.westeurope.cloudapp.azure.com:8080/',
     endpoints: {
-      'http://drmueller.westeurope.cloudapp.azure.com:8081/': '297fc587-359a-4ea5-9f2d-4b36aef9c3a1'
+      'http://drmueller.westeurope.cloudapp.azure.com:80/': '297fc587-359a-4ea5-9f2d-4b36aef9c3a1'
     }
   },
   appInfo: {
